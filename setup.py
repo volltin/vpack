bca37a212ee30c6609cb143d460066392488ed0d
Jiyan He <volltin@live.com>
2021-11-29 13:32:56 +0000

    feat: version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vpack",
-    version="0.0.2",
+    version="0.0.3",
     author="Volltin",
     author_email="volltin@live.com",
     description="A package containing many useful utilities",
